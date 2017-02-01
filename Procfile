2976f651f9162cffa464863e4ea5c4cf6594b615
benstoffelen <benstoffelen@gmail.com>
2017-02-01 10:47:05 +0100

    added Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py
+web: python sentimentanalyserpython2/app.py
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 import semantria
 import time
@@ -124,4 +125,5 @@ def get_version():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
